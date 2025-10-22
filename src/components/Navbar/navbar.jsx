038738deefcb5bfc6cf80385b7b322cf6d4b3985
Amr Essam <amressamgodaabdelalim@gmail.com>
2025-10-22 14:39:25 +0300

    navbar

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo2.png";
+
+const Navbar = () => {
+    return (
+     <div className="navbar">
+        <img src={logo} alt="Logo" />
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+        <div className="nav-connect">Connect with me</div>
+     </div>)
+    }
+
+export default Navbar;
